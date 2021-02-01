338a1e9644164182bd3c65a19168f9d58dab81bd
PhillZitt <phill.zitt@gmail.com>
2021-01-31 18:48:51 -0700

    Update mod to use supported harmony patcher

---
+++ FarmerPatches.cs
@@ -0,0 +1,53 @@
+﻿using StardewModdingAPI;
+
+namespace ExperienceMultiplier
+{
+  public class FarmerPatches
+  {
+
+    private static IMonitor Monitor;
+    private static ModConfig Config;
+
+
+    public static void Initialize(IMonitor monitor, ModConfig config)
+    {
+      Monitor = monitor;
+      Config = config;
+    }
+
+    public static void ReloadConfig(ModConfig config)
+    {
+      Config = config;
+    }
+
+    public static bool onGainExperience(StardewValley.Farmer __instance, int which, ref int howMuch)
+    {
+      howMuch *= Config.GeneralMultiplier;
+
+      switch (which)
+      {
+        case 0:
+          howMuch *= Config.FarmingMultiplier;
+          break;
+        case 1:
+          howMuch *= Config.FishingMultiplier;
+          break;
+        case 2:
+          howMuch *= Config.ForagingMultiplier;
+          break;
+        case 3:
+          howMuch *= Config.MiningMultiplier;
+          break;
+        case 4:
+          howMuch *= Config.CombatMultiplier;
+          break;
+        case 5:
+          howMuch *= Config.LuckMultiplier;
+          break;
+      }
+
+      return true;
+    }
+
+  }
+}
---
+++ ModConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace ExperienceMultiplier
+{
+  public class ModConfig
+  {
+    public int GeneralMultiplier { get; set; } = 1;
+    public int CombatMultiplier { get; set; } = 1;
+    public int FarmingMultiplier { get; set; } = 1;
+    public int FishingMultiplier { get; set; } = 1;
+    public int MiningMultiplier { get; set; } = 1;
+    public int ForagingMultiplier { get; set; } = 1;
+    public int LuckMultiplier { get; set; } = 1;
+  }
+}
